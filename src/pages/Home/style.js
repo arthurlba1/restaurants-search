9ae13adca0b9c3ff92f5c51be83df32fed40b451
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-05 00:40:34 -0300

    container style

---
+++ src/pages/Home/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+  background-color: ${({ theme: { colors } }) => colors.background};
+  width: 360px;
+  height: 100vh;
+  overflow-y: scroll;
+`; 
