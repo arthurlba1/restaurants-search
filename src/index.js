1e68a93d21f6335657ba465c51bcdcae1cc41b2d
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-05 01:21:38 -0300

    material text field import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '@material/react-text-field/dist/text-field.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
