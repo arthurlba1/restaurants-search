0bb399f9d0c5c90ded4e136e65703a8a8eb93662
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-05 00:42:42 -0300

    reset style added to project app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import  { ThemeProvider } from 'styled-components;';
+import { Reset } from 'styled-reset';
+import { ThemeProvider } from 'styled-components;';
 import React from 'react';
 
-import Home from './pages/Home'
-import Theme from './theme'
+import Home from './pages/Home';
+import theme from './theme';
 
 import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={ Theme }>
+    <ThemeProvider theme={ theme }>
+      <Reset />
       <Home/>
   </ThemeProvider>
     );
