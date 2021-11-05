feb864616e3da37dee81ba5d3171662a19365c35
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-05 08:40:34 -0300

    slick carousel import

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 
 import '@material/react-material-icon/dist/material-icon.css';
 import '@material/react-text-field/dist/text-field.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.render(
   <React.StrictMode>
