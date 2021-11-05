7edbf266fe2e8e0c354e234196d030710f7b4e03
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-05 08:48:32 -0300

    slick seetings

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,24 @@
 import logo from '../../assets/logo.svg'
 import MaterialIcon from '@material/react-material-icon';
 import React, { useState } from 'react';
+import Slider from "react-slick";
 import TextField, { Input } from '@material/react-text-field';
 
 import { Container, Logo, Search, Wrapper } from './style'
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        speed: 350,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+      };
+
     return (
         <Wrapper>
             <Container>
