f84eec3575218531c7a04d9fac76d54d22bc78a2
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-07 14:27:33 -0300

    restaurant info in card

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';  
 
-import { Restaurant } from './styles';
+import { Address, Restaurant, RestaurantInfo, Title } from './styles';
 
-const RestaurantCard = () => <Restaurant />;
+const RestaurantCard = () => (
+    <Restaurant>
+        <RestaurantInfo>
+            <Title>Restaurant Name</Title>
+            <p>Evaluation</p>
+            <Address>Address</Address>
+        </RestaurantInfo>
+    </Restaurant>
+)
 
 export default RestaurantCard;
