84c5355e49ed910b531ebddef0d405299213233d
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-08 02:11:03 -0300

    store redux in app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Reset } from 'styled-reset';
+import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components';
 import React from 'react';
 
 import Home from './pages/Home';
+import store from './redux/store'
 import theme from './theme';
 
 import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={ theme }>
-      <Reset />
-      <Home />
-  </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={ theme }>
+        <Reset />
+        <Home />
+    </ThemeProvider>
+    </Provider>
     );
 }
 
