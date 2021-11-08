e2dd094335f61dce7165c6532c8295df66440c58
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-08 02:30:10 -0300

    render carrousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,57 +1,120 @@
-import logo from '../../assets/logo.svg'
-import MaterialIcon from '@material/react-material-icon';
+/* eslint-disable camelcase */
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-import { Card, Map, Modal, RestaurantCard } from '../../components'; 
-import { Carousel, Container, Logo, Search, Wrapper } from './style';
+import {
+  RestaurantCard,
+  Modal,
+  Map,
+  ImageCard,
+  Loader,
+  Text,
+  ImageSkeleton as Skeleton,
+} from '../../components';
+import logo from '../../assets/logo.svg';
+import { Container, Search, Logo, Title, Carousel, Wrapper } from './styles';
 
 const Home = () => {
-    const [inputValue, setInputValue] = useState('');
-    const [query, setQuery] = useState('');
-    const [modalOpened, setModalOpened] = useState(false);
+  const [value, setValue] = useState('');
+  const [query, setQuery] = useState('');
+  const [placeId, setPlaceId] = useState(null);
+  const [open, setOpen] = useState(false);
+  const { restaurants, restaurantSelected } = useSelector((state) => state.restaurants);
+  const hasRestaurants = restaurants.length > 0;
 
-    const settings = {
-        dots: false,
-        infinite: true,
-        autoplay: true,
-        speed: 350,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        adaptiveHeight: true,
-      };
-    
-    const handleChange = (e) => {
-        setValue(e.target.value);
-      };
-    
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            setQuery(value);
-        }
-    };
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
 
-    return (
-        <Wrapper>
-            <Container>
-                <Search>
-                    <Logo src={logo} alt="web app logo"/>
-                    <TextField
-                        outlined
-                        label="Search"
-                        trailingIcon={<MaterialIcon role="button" icon="search" />}>
-                         <Input type="text" value={value} onKeyPress={handleKeyPress} onChange={handleChange} />
-                    </TextField>
-                    <Carousel>
-                        
-                    </Carousel>
-                </Search>
-                <RestaurantCard />
-            </Container>
-            <Map query={query} />
-            <Modal open={modalOpened} onClose ={() => setModalOpened(!modalOpened)} />
-        </Wrapper>
-    );
+  const renderCarousel = () => {
+    if (hasRestaurants) {
+      return (
+        <>
+          <Title size="large">Na sua Área</Title>
+          <Carousel {...settings}>
+            {restaurants.map((restaurant) => (
+              <ImageCard key={restaurant.place_id} restaurant={restaurant} />
+            ))}
+          </Carousel>
+        </>
+      );
+    }
+    return <Loader />;
+  };
+
+  const renderRestaurants = () => {
+    if (hasRestaurants) {
+      return restaurants.map((restaurant) => (
+        <RestaurantCard
+          key={restaurant.place_id}
+          restaurant={restaurant}
+          onClick={() => {
+            setPlaceId(restaurant.place_id);
+            setOpen(true);
+          }}
+        />
+      ));
+    }
+    return null;
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      setQuery(value);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="logo da empresa" />
+          <TextField
+            outlined
+            label="Pesquisar"
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input type="text" value={value} onKeyPress={handleKeyPress} onChange={handleChange} />
+          </TextField>
+          {renderCarousel()}
+        </Search>
+        {renderRestaurants()}
+        <Modal open={open} onClose={() => setOpen(false)}>
+          {restaurantSelected ? (
+            <>
+              <Text size="large">{restaurantSelected?.name}</Text>
+              <Text size="medium">{restaurantSelected?.formatted_phone_number}</Text>
+              <Text size="medium">{restaurantSelected?.formatted_address}</Text>
+              <Text size="medium">
+                {restaurantSelected?.opening_hours?.open_now
+                  ? 'Aberto agora :)'
+                  : 'Fechado neste momento :('}
+              </Text>
+            </>
+          ) : (
+            <>
+              <Skeleton width="10px" height="10px" />
+              <Skeleton width="10px" height="10px" />
+              <Skeleton width="10px" height="10px" />
+              <Skeleton width="10px" height="10px" />
+            </>
+          )}
+        </Modal>
+      </Container>
+      <Map query={query} placeId={placeId} />
+    </Wrapper>
+  );
 };
 
 export default Home;
