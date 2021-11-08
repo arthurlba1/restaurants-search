e80773ce8cff9890e03cb856cee8daf9ae4f46fd
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-08 02:30:56 -0300

    all styleshome page

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import Slider from 'react-slick';
 
+import { Text } from '../../components';
+
 export const Container = styled.aside`
   background-color: ${({ theme: { colors } }) => colors.background};
   width: 360px;
@@ -14,18 +16,22 @@ export const Search = styled.section`
   justify-content: center;
   background-color: #ffffff;
   padding: 16px;
-`; 
+`;
 
 export const Logo = styled.img`
   margin: 15px;
-`; 
+`;
 
-export const Wrapper = styled.div`
-  display: flex;
-`; 
+export const Title = styled(Text)`
+  margin: 16px 0;
+`;
 
 export const Carousel = styled(Slider)`
   .slick-slide {
     margin-right: 16px;
   }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
 `;
