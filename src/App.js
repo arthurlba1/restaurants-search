d8b963b267bf8a54b4144ff878d52ae427ec7ab1
arthurlba1 <1arthurluiz1@gmail.com>
2021-11-08 02:34:18 -0300

    app js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import { Reset } from 'styled-reset';
-import { Provider } from 'react-redux'
-import { ThemeProvider } from 'styled-components';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 
 import Home from './pages/Home';
-import store from './redux/store'
 import theme from './theme';
-
-import './App.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={ theme }>
-        <Reset />
-        <Home />
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <Home />
     </ThemeProvider>
-    </Provider>
-    );
+  );
 }
 
 export default App;
